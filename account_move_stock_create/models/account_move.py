3d44e626f4b3a0a362454cc5cfc4c23c8b178295
Diego Paradeda <diego.paradeda@kmee.com.br>
2024-10-29 16:14:40 -0300

    [ADD] account_move_stock_create: new addon

---
+++ account_move_stock_create/models/account_move.py
@@ -0,0 +1,100 @@
+# Copyright (C) 2024-Today - KMEE (<http://www.kmee.com.br>).
+# Author: Diego Paradeda <diego.paradeda@kmee.com.br>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import _, models
+from odoo.exceptions import UserError
+
+INVOICE_TYPE_MAP = {
+    # Account Move Type | Picking Type Code | Local Origin Usage | Local Dest Usage
+    "in_invoice": ("incoming", "supplier", "internal"),
+    "in_refund": ("outgoing", "internal", "supplier"),
+    "out_invoice": ("outgoing", "internal", "customer"),
+    "out_refund": ("incoming", "customer", "internal"),
+}
+
+
+class AccountInvoice(models.Model):
+    _inherit = "account.move"
+
+    def action_generate_pickings_from_invoices(self):
+        """Generate pickings from invoices."""
+        for record in self:
+            if record.picking_ids and len(self) == 1:
+                raise UserError(
+                    _("There's already a picking created for this account move.")
+                )
+            elif record.picking_ids:
+                continue
+            record.generate_picking_from_invoice()
+
+    def generate_picking_from_invoice(self):
+        """Generate a picking from the invoice."""
+        self.ensure_one()
+        if self.move_type not in INVOICE_TYPE_MAP:
+            raise UserError(f"Unsupported move type: {self.move_type}")
+
+        picking_type_code, location_src_usage, location_dest_usage = INVOICE_TYPE_MAP[
+            self.move_type
+        ]
+
+        # Picking type
+        picking_type_id = self.env["stock.picking.type"].search(
+            [("code", "=", picking_type_code)],
+            limit=1,
+        )
+
+        # Locations
+        location_src_id = self.env["stock.location"].search(
+            [("usage", "=", location_src_usage)],
+            limit=1,
+        )
+        location_dest_id = self.env["stock.location"].search(
+            [("usage", "=", location_dest_usage)],
+            limit=1,
+        )
+
+        # Picking
+        picking_values = {
+            "partner_id": self.partner_id.id,
+            "picking_type_id": picking_type_id.id,
+            "location_id": location_src_id.id,
+            "location_dest_id": location_dest_id.id,
+            "invoice_ids": self.ids,
+            "origin": self.name,
+        }
+
+        # Moves
+        move_values = [
+            self._prepare_stock_move_values(line, picking_values)
+            for line in self.invoice_line_ids
+        ]
+        picking_values["move_lines"] = [(0, 0, move) for move in move_values]
+
+        self._create_picking(picking_values=picking_values)
+
+    def _prepare_stock_move_values(self, invoice_line, picking_values):
+        """Prepare stock move values from invoice line."""
+        self.ensure_one()
+        return {
+            "name": invoice_line.product_id.name,
+            "product_id": invoice_line.product_id.id,
+            "location_id": picking_values.get("location_id"),
+            "location_dest_id": picking_values.get("location_dest_id"),
+            "state": "draft",
+            "company_id": self.company_id.id,
+            "product_uom_qty": invoice_line.quantity,
+            "product_uom": invoice_line.product_uom_id.id,
+            "invoice_line_ids": invoice_line.ids,
+        }
+
+    def _create_picking(self, picking_values):
+        """Create a picking with the given values.
+
+        Override this method if you need to change any values of the
+        picking and the lines before the picking creation.
+
+        :param picking_values: dict with the picking and its lines
+        :return: picking
+        """
+        return self.env["stock.picking"].create(picking_values)
